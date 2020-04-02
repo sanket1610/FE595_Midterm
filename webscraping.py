e98a0f831d8eb741a49056e9475ba0d0d5e161fc
lmurphy1 <34901417+lmurphy1@users.noreply.github.com>
2020-04-02 14:44:19 -0400

    Update webscraping.py

--- webscraping.py
+++ webscraping.py
@@ -8,7 +8,9 @@ import re
 
 
 def extract_source(url):
-    headers = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36"
+    }
     source = requests.get(url, headers=headers).text
     return source
 
@@ -16,7 +18,12 @@ def extract_source(url):
 def get_headlines():
     df = pd.DataFrame()
     for i in range(1, 16):
-        s = extract_source("https://seekingalpha.com/symbol/TSLA/more_focus?page=" + str(i) + "&new_layout=true")
+        s = extract_source(
+            "https://seekingalpha.com/symbol/TSLA/more_focus?page="
+            + str(i)
+            + "&new_layout=true"
+        )
+        # https://stackoverflow.com/questions/54496863/python-parse-html-with-escape-characters
         soup = BeautifulSoup(s.replace(r"\"", '"').replace(r"\/", "/"), "html.parser")
         df = df.append(bs_get(soup))
         time.sleep(randint(5, 15))
@@ -29,10 +36,10 @@ def format_date(x):
     elif x.startswith("Yesterday"):
         c = date.today() - timedelta(days=1)
     else:
-        match = re.search(r'(\D{3}\.\ \d{1,2})$',x)
+        match = re.search(r"(\D{3}\.\ \d{1,2})$", x)
         if match is not None:
-            x = match[0] + ', 2020'
-        c = datetime.strptime(x, '%b. %d, %Y')
+            x = match[0] + ", 2020"
+        c = datetime.strptime(x, "%b. %d, %Y")
     c = c.strftime("%-m/%-d/%Y")
     return c
 
@@ -47,10 +54,10 @@ def bs_get(soup):
         d = con.find("span", class_="date").get_text()
         d = format_date(d)
         dates.append(d)
-    df = pd.DataFrame({'Headlines': headlines, 'Dates': dates})
+    df = pd.DataFrame({"Headlines": headlines, "Dates": dates})
     return df
 
 
 if __name__ == "__main__":
     df = get_headlines()
-    df.to_csv("TSLA.csv", index = False)
+    df.to_csv("TSLA.csv", index=False)
