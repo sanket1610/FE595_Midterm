0e2c6ee972f49ad37758ad6843e6a664a6463fed
arunvinesh <44319612+arunvinesh@users.noreply.github.com>
2020-05-10 21:23:59 -0400

    Final merged code

--- Federico.Analysis.py
+++ Federico.Analysis.py
@@ -1,15 +1,21 @@
+import os
+import requests
+import operator
+import re
+import nltk
+from flask import Flask, render_template, request
 import nltk
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
-# from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score
 from sklearn import svm
 from sklearn.naive_bayes import GaussianNB, BernoulliNB
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
+#x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
 
 nltk.download("vader_lexicon")
 import pandas as pd
@@ -18,6 +24,8 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer as vader_sentiment
 from pandas_datareader import data as pdr
 import yfinance as yfinance_reader
 from sklearn import preprocessing
+
+
 
 
 def update_sentiment_value(input_df_sentiment):
@@ -84,35 +92,46 @@ def knc(x_train, x_test, y_train, y_test):
     model1 = KNeighborsClassifier(algorithm='ball_tree')
     model1.fit(x_train, y_train)
     predictions = model1.predict(x_test)
-    print("knn score ", accuracy_score(y_test, predictions) * 100)
+    knc_score = accuracy_score(y_test, predictions) * 100
+    return(knc_score)
+
 
 
 def logisticreg(x_train, x_test, y_train, y_test):
     model2 = LogisticRegression(solver='newton-cg', multi_class='ovr', max_iter=200, penalty='l2')
     model2.fit(x_train, y_train)
     predictions = model2.predict(x_test)
-    print("LogRegression ", accuracy_score(y_test, predictions) * 100)
-
+    LogRegression = accuracy_score(y_test, predictions) * 100
+    return(LogRegression)
 
 def support(x_train, x_test, y_train, y_test):
     model3 = svm.SVC(kernel='sigmoid')
     model3.fit(x_train, y_train)
     predictions = model3.predict(x_test)
-    print("SVM ", accuracy_score(y_test, predictions) * 100)
+    supp = accuracy_score(y_test, predictions) * 100
+    return(supp)
 
 
-def naive(x_train, x_test, y_train, y_test):
+def naive_bayes_gaussian(x_train, x_test, y_train, y_test):
     model_guass = GaussianNB()
-    model4 = BernoulliNB()
     model_guass.fit(x_train, y_train)
-    model4.fit(x_train, y_train)
     predictions_gauss = model_guass.predict(x_test)
+    nbg = accuracy_score(y_test, predictions_gauss) * 100
+    return nbg
+
+
+def naive_bayes_bernoulli(x_train, x_test, y_train, y_test):
+    model4 = BernoulliNB()
+    model4.fit(x_train, y_train)
     predictions = model4.predict(x_test)
-    print("naive bayes using gaussian ", accuracy_score(y_test, predictions_gauss) * 100)
-    print("naive bayes using Bernoulli ", accuracy_score(y_test, predictions) * 100)
+    nbb = accuracy_score(y_test, predictions) * 100
+    return nbb
 
+app = Flask(__name__)
 
-if __name__ == "__main__":
+@app.route('/')
+def home():
+    print('***************************************')
     input_datafile_csv = "TSLA_1.csv"
     df_grabbed_sentiment = pd.read_csv(input_datafile_csv)
     df_grabbed_sentiment = update_sentiment_value(df_grabbed_sentiment)
@@ -124,5 +143,11 @@ if __name__ == "__main__":
     a, b, c, d = preprocessings(merged_df)
     kneigh = knc(a, b, c, d)
     logistic = logisticreg(a, b, c, d)
-    support = support(a, b, c, d)
-    naivebayse = naive(a, b, c, d)
+    supportVector = support(a, b, c, d)
+    naivebgauss = naive_bayes_gaussian(a,b,c,d)
+    naivebberno = naive_bayes_bernoulli(a,b,c,d)
+
+    return render_template('home.html' , data1= kneigh , data2= logistic , data3= supportVector , data4= naivebgauss , data5= naivebberno)
+
+if __name__ == '__main__':
+    app.run(debug=True)
